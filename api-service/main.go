d247f0209781b4119624167830023502d853d7e9
tyler ackerson <tyler.a.ackerson@gmail.com>
2022-03-25 20:20:22 -0700

    Create order, some basic validations

--- api-service/main.go
+++ api-service/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"time"
@@ -32,8 +34,39 @@ func getAllMenuItems(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(res.MenuItemsList)
 }
 
+func createOrder(w http.ResponseWriter, r *http.Request) {
+	conn, err := grpc.Dial("localhost:9090", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	log.Println(" connection state ====> ", conn.GetState())
+
+	c := pb.NewOrderServiceClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	var newOrder pb.CreateOrderRequest
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "Invalid order request")
+	}
+
+	json.Unmarshal(reqBody, &newOrder)
+
+	res, err := c.CreateOrder(ctx, &newOrder)
+	if err != nil {
+		fmt.Fprintf(w, "Failed to create order")
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(res.Id)
+}
+
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/orders", createOrder).Methods("POST")
 	router.HandleFunc("/menu-items", getAllMenuItems).Methods("GET")
 	log.Fatal(http.ListenAndServe("localhost:8333", router))
 }

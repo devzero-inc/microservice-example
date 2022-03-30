69f065ceb88e9dd57dbaa929401df5e4f348b5e2
jason.gonzales23@gmail.com <>
2022-03-30 23:31:59 +0000

    first commit

---
+++ cafe-web-client/pages/index.js
@@ -0,0 +1,12 @@
+import Layout from '../components/Layout'
+import { Box, Typography } from "@mui/material"
+
+export default function Home() {
+  return (
+    <Layout>
+      <Box>
+        <Typography>Hello</Typography>
+      </Box>
+    </Layout>
+  )
+}

9f05a91c31b3b622bb446a5a914b4c60863c919c
jason.gonzales23@gmail.com <>
2022-03-31 21:50:40 +0000

    trying the http client

--- web-client/components/MenuItems.tsx
+++ web-client/components/MenuItems.tsx
@@ -2,6 +2,8 @@ import MenuItem from "./MenuItem";
 import { ModelMenuItem } from "./MenuItem";
 
 export default function MenuItems({ data }: ModelMenuItem[]) {
+  if (!data) return null;
+  console.log(data);
   return data.map((item: ModelMenuItem) => (
     <MenuItem key={item.id} {...item} />
   ));

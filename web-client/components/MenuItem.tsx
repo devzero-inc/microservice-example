8e0b5b23824f153ae5f704c8d3ef5799d6e20d18
jason.gonzales23@gmail.com <>
2022-03-31 21:41:51 +0000

    layout menu item cards

--- web-client/components/MenuItem.tsx
+++ web-client/components/MenuItem.tsx
@@ -1,16 +1,45 @@
-import { Grid, Card, Typography } from "@mui/material";
+import {
+  Button,
+  Grid,
+  Card,
+  CardActions,
+  CardContent,
+  Typography,
+} from "@mui/material";
 
 export type ModelMenuItem = {
   id: number;
   name: string;
   description: string;
 };
+
+const devZeroYellow2 = "#C3B146";
+const devZeroYellow1 = "#D3BD5A";
 export default function MenuItem({ id, name, description }: ModelMenuItem) {
   return (
     <Grid item md={4} lg={4}>
-      <Card sx={{ mt: 2, p: 2, height: 200 }}>
-        <Typography>{name}</Typography>
-        <Typography>{description}</Typography>
+      <Card
+        sx={{
+          mt: 2,
+          p: 2,
+          height: 200,
+          borderTop: `8px solid ${devZeroYellow1}`,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+        }}
+      >
+        <CardContent>
+          <Typography variant="h5" gutterBottom>
+            {name}
+          </Typography>
+          <Typography variant="body2">{description}</Typography>
+        </CardContent>
+        <CardActions>
+          <Button variant="contained" color="success">
+            Add to cart
+          </Button>
+        </CardActions>
       </Card>
     </Grid>
   );

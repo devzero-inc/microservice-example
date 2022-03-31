488d20f2ba4ffc88dc2d57e39e07e0765a0fa6fe
jason.gonzales23@gmail.com <>
2022-03-31 21:50:40 +0000

    trying the http client

--- web-client/pages/index.tsx
+++ web-client/pages/index.tsx
@@ -4,16 +4,17 @@ import axios from "axios";
 import MenuItems from "../components/MenuItems";
 
 export default function Home() {
-  // const menuItems = axios.get("http://localhost:8333/menu-items");
-  // console.log(menuItems);
-  const menuItemData = [
-    { id: 1, name: "Drip coffee", description: "Fast, simple, delicious" },
-    { id: 2, name: "Espresso", description: "The potent option" },
-    { id: 3, name: "Cortado", description: "Espresso with a dash of milk" },
-    { id: 4, name: "Gibraltar", description: "Like a Cortado, but different" },
-    { id: 5, name: "Pour over", description: "Fancy drip made by a human" },
-    { id: 6, name: "Americano", description: "Espresso cut with water" },
-  ];
+  const menuItemData = axios.get("http://localhost:8333/menu-items");
+  console.log(menuItemData);
+  // const menuItemData = [
+  //   { id: 1, name: "Drip coffee", description: "Fast, simple, delicious" },
+  //   { id: 2, name: "Espresso", description: "The potent option" },
+  //   { id: 3, name: "Cortado", description: "Espresso with a dash of milk" },
+  //   { id: 4, name: "Gibraltar", description: "Like a Cortado, but different" },
+  //   { id: 5, name: "Pour over", description: "Fancy drip made by a human" },
+  //   { id: 6, name: "Americano", description: "Espresso cut with water" },
+  // ];
+  ///D
   return (
     <Layout>
       <Grid container spacing={2}>

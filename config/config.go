df0233e5b6aa010a3f06df0f4d38cbd8a8871be1
tyler.a.ackerson@gmail.com <>
2022-04-04 19:53:26 +0000

    Remove serv-side CORS stuff

--- config/config.go
+++ config/config.go
@@ -10,9 +10,8 @@ import (
 
 type Config struct {
 	APIService struct {
-		Hostname       string `yaml:"hostname" envconfig:"API_HOSTNAME"`
-		Port           string `yaml:"port" envconfig:"API_PORT"`
-		AllowedOrigins []string `yaml:"allowed_origins" envconfig:"API_ALLOWED_ORIGINS"`
+		Hostname string `yaml:"hostname" envconfig:"API_HOSTNAME"`
+		Port     string `yaml:"port" envconfig:"API_PORT"`
 	} `yaml:"api-service"`
 	BackendService struct {
 		Hostname string `yaml:"hostname" envconfig:"BACKEND_HOSTNAME"`

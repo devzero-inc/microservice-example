5cb4f2e94be9d1e3e23cdee0485599c001735b10
Everton Moreth <everton.moreth@gmail.com>
2023-05-16 15:08:43 -0300

    Revert "Update README.md (#42)"
    This reverts commit 77e05c5900a891558be0f23d45a946e2cd4495f3.

--- web-client/pages/api/create-order.tsx
+++ web-client/pages/api/create-order.tsx
@@ -5,7 +5,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 const createOrder = () => async (req: NextApiRequest, res: NextApiResponse) => {
     try {
-        const response: AxiosResponse = await axios.post("http://localhost:8333/orders", req.body);
+        const response: AxiosResponse = await axios.post(`http://${process.env.API_HOST || 'localhost'}:8333/orders`, req.body);
         res.status(response.status || 200).json(response.data);
     } catch (error: any) {
         console.log(error);

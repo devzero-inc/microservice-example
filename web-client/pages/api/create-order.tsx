f9c3ffaf4955cea9490fe542fc09128c1081730b
Debosmit Ray <dray92@uw.edu>
2023-06-08 03:53:08 +0000

    minor

--- web-client/pages/api/create-order.tsx
+++ web-client/pages/api/create-order.tsx
@@ -5,7 +5,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 const createOrder = () => async (req: NextApiRequest, res: NextApiResponse) => {
     try {
-        const hostname = process.env.API_GW ? process.env.API_GW : 'localhost'
+        const hostname = process.env.API_GW != null ? process.env.API_GW : 'localhost';
         const response: AxiosResponse = await axios.post(`http://${hostname}:8333/orders`, req.body);
         res.status(response.status || 200).json(response.data);
     } catch (error: any) {
--- web-client/pages/api/get-menu-items.tsx
+++ web-client/pages/api/get-menu-items.tsx
@@ -7,7 +7,6 @@ import { NextApiRequest, NextApiResponse } from "next";
 const getMenuItems = () => async (req: NextApiRequest, res: NextApiResponse) => {
     try {
         const hostname = process.env.API_GW != null ? process.env.API_GW : 'localhost';
-        console.log(hostname);
         const response: AxiosResponse = await axios.get(`http://${hostname}:8333/menu-items`);
         res.status(response.status || 200).json(response.data);
     } catch (error: any) {
